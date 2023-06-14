dc77fbb5ec72c12880421d2c6c341fcfae6ee663
Ty Hitzeman <ty@switchback.tech>
2023-06-13 23:48:29 -0500

    init project

---
+++ src/backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const stytch = require('stytch');
+
+const PORT = 3001;
+const STYTCH_PROJECT_ID =  "project-test-d2f1576f-8019-4208-a261-1c1f1306258d"
+const STYTCH_SECRET =  "secret-test-77py-dm7QD54fSUyq1BlUW9QYGiD0r41H6U="
+
+const app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/public/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, '../frontend/public')));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+const client = new stytch.Client({
+  project_id: STYTCH_PROJECT_ID,
+  secret: STYTCH_SECRET,
+  env: stytch.envs.test,
+});
---
+++ src/frontend/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+
+import { StytchProvider } from '@stytch/react';
+import { StytchUIClient } from '@stytch/vanilla-js';
+
+
+
+function App() {
+  return (
+    <div>
+    <header>
+        <h1>Auth Comparisons</h1>
+        <p>A playground to compare how <br/>auth platforms
+        implement session management</p>
+        <a 
+        className="link" 
+        href="https://github.com/tyler-dane/auth-comparisons"
+        rel="noopener noreferrer"
+        target="_blank">
+        See the code
+        </a>
+    </header>
+
+      <div className="container">
+        <div class="column column-header">
+          <a
+            className="link"
+            href="https://stytch.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Stytch 🪡
+          </a>
+          <button>create session</button>
+        </div>
+
+        <div class="column column-header">
+          <a
+            className="link"
+            href="https://supertokens.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Supertokens 🦸‍♀️
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
